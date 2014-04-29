8a14620acd7b4f2ab533114491b2d65241baae2a
Dan Knights <danknights@gmail.com>
2014-04-28 20:02:50 -0500

    fixed path

--- bin/make_parallel_dge_commands.py
+++ bin/make_parallel_dge_commands.py
@@ -4,7 +4,7 @@
 import sys
 
 bsub = 'bsub -o maketable.lsf -q hour -R "rusage[mem=32]" "'
-basecmd = 'Rscript $MWAS_GWAS_DIR/src/bin/run.dge.test.r -i input/taxa.txt -m input/md.txt -x input/gx.txt -a input/gx.annot.txt '
+basecmd = 'Rscript $MWAS_GWAS_DIR/bin/run.dge.test.r -i input/taxa.txt -m input/md.txt -x input/gx.txt -a input/gx.annot.txt '
 
 # read in headers from gx file
 line = open('input/gx.txt','r').readline()
